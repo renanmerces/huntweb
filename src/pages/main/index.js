b2e97583a1426d1a85e5dbdcfc6fad82af7b9fd3
Anunciacao <renan.anunciacao@capgemini.com>
2019-09-27 15:02:20 -0300

    Funções de paginação

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -8,7 +8,9 @@ export default class Main extends Component {
     constructor(props){
         super(props)
         this.state = {
-            products: []
+            products: [],
+            productInfo: {},
+            page: 1
         }
     }
 
@@ -16,15 +18,35 @@ export default class Main extends Component {
         this.loadProducts()
     }
 
-    loadProducts = async () => {
-        const response = await api.get('/products')
+    loadProducts = async (page = 1) => {
+        const response = await api.get(`/products?page=${page}`)
         console.log(response.data)
 
-        this.setState({products: response.data.docs})
+        const {docs, ...productInfo} = response.data
+
+        this.setState({products: docs, productInfo, page})
+    }
+
+    nexPage = () => {
+
+        const {page, productInfo} = this.state
+
+        if(page === productInfo.pages) return
+
+        this.loadProducts(page + 1)
+
+    }
+
+    prevPage = () => {
+        const {page} = this.state
+
+        if(page === 1) return
+
+        this.loadProducts(page - 1)
     }
 
     render(){
-        const { products } = this.state
+        const { products, productInfo, page } = this.state
 
         return(
             <div className="product-list">
@@ -35,6 +57,10 @@ export default class Main extends Component {
                         <a href="#">Acessar</a>
                     </article>
                 ))}
+                <div className="actions">
+                    <button onClick={this.prevPage}>Anterior</button>
+                    <button onClick={this.nexPage}>Próxima</button>
+                </div>
             </div>
         )
     }
